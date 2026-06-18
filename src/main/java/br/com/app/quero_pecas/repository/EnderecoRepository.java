70cf2dc6e28c3cad0f17246420bf7cce2a8b4886
Gilmar-Viriato <113316210+Gilmar-Viriato@users.noreply.github.com>
2026-06-18 18:48:10 -0300

    Delete src/main/java/br/com/app/quero_pecas/repository/EnderecoRepository.java

--- src/main/java/br/com/app/quero_pecas/repository/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.app.quero_pecas.repository;
-
-import br.com.app.quero_pecas.entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-
-}
